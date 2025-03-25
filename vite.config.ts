de193b1f894d7c6e7fbc89e1c42bf2b27f6b1420
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-03-25 03:35:19 +0200

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/gemini-quiz-app/',
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
